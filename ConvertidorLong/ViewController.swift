ec09f735df423c587bbc8a7eb322bbd6ff6dc738
Alek Howland <83378924+HowlandAlek@users.noreply.github.com>
2021-10-25 23:26:48 -0500

    Initial commit

---
+++ ConvertidorLong/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ConvertidorLong
+//
+//  Created by Alek Howland on 25/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    // Configurar Outlets
+    
+    @IBOutlet weak var tfPulgadas: UITextField!
+    
+    @IBOutlet weak var tfCentimetros: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
